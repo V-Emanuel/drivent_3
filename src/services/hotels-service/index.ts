328d53faab978cb23988a00cec1f276029e257d9
V-Emanuel <gvictor567@gmail.com>
2023-04-24 10:00:59 -0300

    feat: tests /hotels/:idHotel

--- src/services/hotels-service/index.ts
+++ src/services/hotels-service/index.ts
@@ -1,9 +1,24 @@
 import { Hotel } from '@prisma/client';
+import httpStatus from 'http-status';
 import hotelsRepository from '@/repositories/hotels-repository';
+import ticketsRepository from '@/repositories/tickets-repository';
+import enrollmentRepository from '@/repositories/enrollment-repository';
 import { notFoundError } from '@/errors';
 
 async function getHotel(userId: number): Promise<Hotel[]> {
   const hotels: Hotel[] = await hotelsRepository.findHotels();
+  if (!hotels) throw notFoundError();
+  if (hotels.length == 0) throw notFoundError();
+
+  const enrollment = await enrollmentRepository.findUserById(userId);
+  if (!enrollment) throw notFoundError();
+  const ticket = await ticketsRepository.findTicketByEnrollmentId(enrollment.id);
+  if (!ticket) throw notFoundError();
+
+  if (ticket.TicketType.isRemote == true) throw httpStatus.PAYMENT_REQUIRED;
+  if (ticket.TicketType.includesHotel == false) throw httpStatus.PAYMENT_REQUIRED;
+  if (ticket.status != 'PAID') throw httpStatus.PAYMENT_REQUIRED;
+
   return hotels;
 }
 
--- tests/integration/hotels.test.ts
+++ tests/integration/hotels.test.ts
@@ -3,7 +3,7 @@ import faker from '@faker-js/faker';
 import httpStatus from 'http-status';
 import * as jwt from 'jsonwebtoken';
 import { cleanDb, generateValidToken } from '../helpers';
-import { createEnrollmentWithAddress, createUser, createTicketType, createTicket } from '../factories';
+import { createEnrollmentWithAddress, createUser, createHotel } from '../factories';
 import app, { init } from '@/app';
 
 beforeAll(async () => {
@@ -26,7 +26,7 @@ describe('GET /hotels', () => {
     expect(result.status).toBe(httpStatus.UNAUTHORIZED);
   });
 });
-describe('Quando o token é válido', () => {
+describe('/hotels Quando o token é válido', () => {
   it('Retorna 200 se o token é válido', async () => {
     const token = await generateValidToken();
     const result = await server.get('/hotels').set('Authorization', `Bearer ${token}`);
@@ -43,26 +43,25 @@ describe('Quando o token é válido', () => {
       ]),
     );
   });
-  it('Retorna um array vazio quando não há hotéis', async () => {
-    const user = await createUser();
-    const token = await generateValidToken(user);
-    const result = await server.get('/hotels').set('Authorization', `Bearer ${token}`);
-    expect(result.body).toEqual([]);
+});
+describe('GET hotels/:hotelId', () => {
+  it('Retorna 401 se o token passado está incorreto', async () => {
+    const token = faker.lorem.word();
+    const createdHotel = await createHotel();
+    const result = await server.get(`/hotels/${String(createdHotel.id)}`).set('Authorization', `Bearer ${token}`);
+    expect(result.status).toBe(httpStatus.UNAUTHORIZED);
+  });
+  it('Retorna 401 se não foi passado token', async () => {
+    const createdHotel = await createHotel();
+    const result = await server.get(`/hotels/${String(createdHotel.id)}`);
+    expect(result.status).toBe(httpStatus.UNAUTHORIZED);
   });
 });
-
-{
-  /*
-    expect(result.body).toEqual(
-            expect.arrayContaining([
-                expect.objectContaining({
-                    id: expect.any(Number),
-                    name: expect.any(String),
-                    image: expect.any(String),
-                    createdAt: expect.any(String),
-                    updatedAt: expect.any(String)
-                })
-            ])
-        );
-*/
-}
+describe('hotels/:hotelId Quando o token é válido', () => {
+  it('Retorna 200 se o token é válido', async () => {
+    const token = await generateValidToken();
+    const createdHotel = await createHotel();
+    const result = await server.get(`/hotels/${String(createdHotel.id)}`).set('Authorization', `Bearer ${token}`);
+    expect(result.status).toEqual(httpStatus.OK);
+  });
+});
